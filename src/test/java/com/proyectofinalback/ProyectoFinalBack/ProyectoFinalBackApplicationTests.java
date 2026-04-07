c9d926d36565a41dfbaa259232ee722e422f6dcb
JoseAntonio1DAM <usr4034@salesianos-lacuesta.net>
2026-04-07 19:46:41 +0100

    feat: añadidas las entidades del repo de prueba

---
+++ src/test/java/com/proyectofinalback/ProyectoFinalBack/ProyectoFinalBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectofinalback.ProyectoFinalBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
